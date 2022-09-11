72fb433f55e32772b8815e29cde7e6394c2d1ede
A. Peixinho <a_peixinho@sapo.pt>
2022-09-11 15:12:25 +0200

    Changed autotype message. Changed pdf filename to URL safe. Upgraded babel minor version

--- src/scripts/autotype.js
+++ src/scripts/autotype.js
@@ -1,4 +1,4 @@
-var dataset = ["Hi, and welcome to my portfolio site.", "I'm a passionate full-stack developer.", "Always eager to learn about new technologies and paradigms."];
+var dataset = ["Hi, and welcome to my portfolio site.", "I'm software developer enthusiast.", "Always eager to learn about new technologies and paradigms."];
 
 var datasetIndex = 0;
 var data;
